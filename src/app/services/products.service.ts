be96217bb5b74fcb1467ea3fafc1557947a00711
candyluux <candelombardo04@gmail.com>
2024-06-21 19:56:17 -0300

    Cree mi rama y modifique la app de ionic

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -23,7 +23,7 @@ export class ProductsService {
 
   getAll(): Promise<Product[]> {
     return firstValueFrom(
-      this.httpClient.get<Product[]>('https://6662036463e6a0189fec7284.mockapi.io/products')
+      this.httpClient.get<Product[]>('https://6668b487f53957909ff8f14f.mockapi.io/products')
     )
   }
 
